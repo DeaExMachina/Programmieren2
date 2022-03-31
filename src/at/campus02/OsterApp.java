018c89ebd8372dfe004f6c18c32c0df14c43846d
DeaExMachina <sister_of_night@gmx.at>
2022-03-31 11:34:13 +0200

    .

--- src/at/campus02/OsterApp.java
+++ src/at/campus02/OsterApp.java
@@ -2,11 +2,17 @@ package at.campus02;
 
 public class OsterApp {
     public static void main(String[] args) {
-        Osterhasen dieter = new Osterhasen();
+        Osterhasen dieter = new Osterhasen("Dieter (formerly known as Hansi)", 10);
+        Osterhasen marla = new Osterhasen("Marla");
 
-        dieter.aendereAnzahlEier(5);
-        System.out.println(dieter.anzahlVersteckteEier);
-        dieter.aendereAnzahlEier();
-        System.out.println(dieter.anzahlVersteckteEier);
+//        dieter.setAnzahlVersteckteEier(5);
+//        dieter.setName("Dieter (formerly known as Hansi)");
+        System.out.println(dieter.getAnzahlVersteckteEier());
+        System.out.println(dieter.getName());
+        System.out.println(marla.getName());
+
+        //möchte ein Ei zur Anzahl der Eier dazuzählen
+        dieter.setAnzahlVersteckteEier(dieter.getAnzahlVersteckteEier() + 1); //weil wir keine Variablennamen haben
+        System.out.println(dieter.getAnzahlVersteckteEier());
     }
 }
--- src/at/campus02/Osterhasen.java
+++ src/at/campus02/Osterhasen.java
@@ -2,21 +2,57 @@ package at.campus02;
 
 public class Osterhasen {
 
+    private String name;
     public int anzahlVersteckteEier;
 
-    public void aendereAnzahlEier(int anzahlVersteckteEier) {
-        // anzahlVersteckteEier --> lokale Variable oder Parameter mit dem gleichen Namen wie oben; bevorzugt
-        // this-Referenz zeigt auf aktuelle Instanz
+    public Osterhasen(String name, int anzahlVersteckteEier) { //Konstruktor
+        this.name = name;
         this.anzahlVersteckteEier = anzahlVersteckteEier;
     }
+
+    public Osterhasen(String name){
+        this.name = name;
+        this.anzahlVersteckteEier = 0;
+    }
+
+//    public void aendereAnzahlEier(int anzahlVersteckteEier) {
+//        // anzahlVersteckteEier --> lokale Variable oder Parameter mit dem gleichen Namen wie oben; bevorzugt
+//        // this-Referenz zeigt auf aktuelle Instanz
+//        this.anzahlVersteckteEier = anzahlVersteckteEier;
+//    }
 
     // Methoden überladen - unterscheiden sich durch Typ/Anzahl der Parameter
     // hier haben wir eine ohne Parameter - Osterhase soll ein Dutzend Eier verteilen
 
-    public void aendereAnzahlEier() {
-        anzahlVersteckteEier = 12;
+//    public void aendereAnzahlEier() {
+//        anzahlVersteckteEier = 12;
         //this.anzahl... wäre möglich, aber nicht notwendig, da keine Namensgleichheit
+//    }
+
+//    public void gibMirAnzahlDerVerstecktenEier (int anzahlVersteckteEier) {
+//        if (anzahlVersteckteEier >= 0) {
+//            this.anzahlVersteckteEier = anzahlVersteckteEier;
+//        }
+//    }
+
+    // Getter und Setter über Code - Generate - Getter and Setter generieren
+
+    public int getAnzahlVersteckteEier() {
+        return anzahlVersteckteEier;
     }
 
+    public void setAnzahlVersteckteEier(int anzahlVersteckteEier) {
+        if (anzahlVersteckteEier >= 0) {
+            this.anzahlVersteckteEier = anzahlVersteckteEier;
+        }
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+

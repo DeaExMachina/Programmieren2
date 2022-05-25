e547bea2cf75331177f8566cd3d64430f09fd7d8
DeaExMachina <sister_of_night@gmx.at>
2022-05-25 14:39:24 +0200

    .

---
+++ src/at/campus02/AccountSorting.java
@@ -0,0 +1,21 @@
+package at.campus02;
+
+import java.util.ArrayList;
+
+public class AccountSorting {
+    public static void main(String[] args) {
+
+
+        Account a1 = new Account("Mein", "AT2584681", "54841325");
+        Account a2 = new Account("Ich", "ATmhuhbuhd25", "545452");
+        Account a3 = new Account("Dei Mama", "AT44878", "1764632");
+        a1.add(1000);
+        a2.add(400);
+        a3.add(200);
+
+        ArrayList<Account> allAccounts = new ArrayList<>();
+        allAccounts.add(a1);
+        allAccounts.add(a2);
+        allAccounts.add(a3);
+    }
+}
---
+++ src/at/campus02/uebungen2/EmployeeManager.java
@@ -0,0 +1,42 @@
+package at.campus02.uebungen2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeManager {
+
+    List <Employee> employees;
+
+    public EmployeeManager(){
+         employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee e){
+        employees.add(e);
+
+    }
+
+    public Employee findByEmpNumber (int number){
+
+    }
+
+
+
+
+}
+
+
+
+
+        – Employee findByEmpNumber(int number) ... soll den Mitarbeiter
+        basierend auf seiner Mitarbeiternummer finden. Falls diese nicht existiert,
+        soll null zurück geliefert werden.
+        – ArrayList<Employee> findByDepartment(String department)
+        ... soll alle Mitarbeiter finden, welche in der angegebenen
+        Abteilung arbeiten.
+        – Employee findByMaxSalary() ... soll den Mitarbeiter mit dem höchsten
+        Gehalt finden, falls mehrere mit dem gleichen Gehalt vorhanden, soll
+        der zuerst gefundene Mitarbeiter geliefert werden
+        Schreiben Sie auch eine einfache DemoEmp Klasse samt main Methode, in welcher
+        Sie einige Employee Objekte instanzieren sowie die Funktionalität des
+        EmployeeManager zeigen
